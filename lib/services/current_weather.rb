2d1a01c8649b74011cb85ca15e0248a9c4adc5a1
Tech-A25 <Tech-A25@Tech-A25s-MacBook-Air.local>
2019-01-31 12:29:51 +0000

    Put in the method to get data through ID and doing the test for current weather

--- lib/services/current_weather.rb
+++ lib/services/current_weather.rb
@@ -1,23 +1,34 @@
 require 'httparty'
 require 'json'
 require 'dotenv'
+Dotenv.load('../../.env')
 
 class Current_Weather
   include HTTParty
 
+  attr_accessor :latest_weather_data
+
   base_uri 'https://api.openweathermap.org/data/2.5'
 
-  def retrieve_current_weather(city_name)
-    api_key = '4e0c46c583623427c8a51f370e0dddb8'
+  def retrieve_current_weather_name(city_name)
+    api_key = ENV['API_KEY']
     @latest_weather_data = JSON.parse(self.class.get("/weather?q=#{city_name}&appid=#{api_key}").body)
   end
+
+  def retrieve_current_weather_id(city_id)
+    api_key = ENV['API_KEY']
+    @latest_weather_data = JSON.parse(self.class.get("/weather?id=#{city_id}&appid=#{api_key}").body)
+  end
+
+  def retrieve_results
+
+  end 
 
   def print_results
     print @latest_weather_data
   end
-
 end
 
 test = Current_Weather.new
-test.retrieve_current_weather('Manchester')
+test.retrieve_current_weather_name('London')
 test.print_results
